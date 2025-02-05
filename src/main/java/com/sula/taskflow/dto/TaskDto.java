4754e851386512affee58097ef335ecfd19c5325
Sulakshana Ranawake <sula.ranawake@gmail.com>
2025-02-06 00:38:07 +0530

    Task Dto added

---
+++ src/main/java/com/sula/taskflow/dto/TaskDto.java
@@ -0,0 +1,11 @@
+package com.sula.taskflow.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskDto {
+    private String title;
+    private String description;
+}
